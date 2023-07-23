f35110565b8adea69389326f4e5c8ae6904d3c44
Moin-M <moinudhinhasan@gmail.com>
2023-07-23 17:00:39 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detects faulty wafer of a semiconductor',
+    author='Moin-M',
+    license='MIT',
+)
